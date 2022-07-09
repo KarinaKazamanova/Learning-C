fd64be956aeb9969dfa9d9407e2fe2e8b80f08ec
Karina <k5524392@gmail.com>
2022-07-09 14:59:40 +0300

    решение задачи 2

---
+++ Семинар 1/Задача 2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число a");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число b");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a*a == b)
+{
+    Console.WriteLine(b + " является квадратом " + a);
+}
+else if (b*b == a)
+{
+    Console.WriteLine(a + " является квадратом " + b);
+}
+else 
+{
+    Console.WriteLine("Квадратов нет");
+}
