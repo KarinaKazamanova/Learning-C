a266e0dd7af483b1d68d404a9fccf6fc576ad24e
Karina <k5524392@gmail.com>
2022-07-09 17:53:06 +0300

    решение задачи 13

---
+++ Семинар 1/Задача 13/Program.cs
@@ -0,0 +1,4 @@
+﻿// Удалить вторую цифру целого числа введенного с клавиатуры.
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"{a/100}{a % 10}");
