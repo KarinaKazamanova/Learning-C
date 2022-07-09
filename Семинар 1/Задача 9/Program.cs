96f9a046213151fd00830d41f8551c9d4be7eaa7
Karina <k5524392@gmail.com>
2022-07-09 16:34:19 +0300

    решение задачи 9

---
+++ Семинар 1/Задача 9/Program.cs
@@ -0,0 +1,12 @@
+﻿// Вывести на экран четные числа от 1 до N
+Console.WriteLine("Введите число N");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+while (i < N)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write($"{i} ");
+    }
+    i++;
+}
