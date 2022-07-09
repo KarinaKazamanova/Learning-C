d808d85ca9729c5288f43f7ae37b24dd1904a2ac
Karina <k5524392@gmail.com>
2022-07-09 20:49:02 +0300

    решение задачи 4

--- Семинар 1/Задача 4/Program.cs
+++ Семинар 1/Задача 4/Program.cs
@@ -1,6 +1,4 @@
-﻿// По заданному с клавиатуры номеру дня недели вывести его название 
-private void MyFunc(bool stop = false;)
-{
+﻿// По заданному с клавиатуры номеру дня недели вывести его название.
 Console.WriteLine("Введите число от 1 до 7");
 int a = Convert.ToInt32(Console.ReadLine());
 if (a == 1)
@@ -31,6 +29,8 @@ else if (a == 7)
 {
     Console.WriteLine("Воскресенье");
 }
-
-MyFunc(); 
+else
+{
+    Console.WriteLine("Введено неверное число. Введите число от 1 до 7");
 }
+
