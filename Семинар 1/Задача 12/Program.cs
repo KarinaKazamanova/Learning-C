8c02c293d9369ebc6a4e91f76c2d3cf2610b074b
Karina <k5524392@gmail.com>
2022-07-09 20:34:37 +0300

    решение задачи 12

---
+++ Семинар 1/Задача 12/Program.cs
@@ -0,0 +1,34 @@
+﻿// Дано число из  диапозона  [10, 99]. Показать наибольшую цифру числа
+/*
+Console.WriteLine("Введите число");
+string s = Console.ReadLine();
+int[] digmas = new int[s.Length];
+for (int i = 0; i < s.Length; i++)
+{
+    digmas[i] = s[i];
+}
+int d = digmas.Max<int>();
+Console.WriteLine($"Максимальное число = {d}");
+
+!!! Пробовала через массив решить, чтобы код был более универсальным (для любого введенного числа, но с дальнейшим добавлением условия при котором при введении числа вне диапозона выдавалась бы "Error"), но на строчке 8 значения s[i] увеличиваются на 48 при добавлении в массив digmas, соотвественно ответ всегда на 48 больше выходит.
+
+*/
+Console.WriteLine("Введите число в диапозоне [10, 99]");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = a / 10;
+int c = a % 10;
+if (a > 10 && a < 100)
+{    
+    if (b >= c)
+    {
+        Console.WriteLine($"Наибольшая цифра = {b}");
+    }
+    else if (b < c)
+    {
+        Console.WriteLine($"Наибольшая цифра = {c}");
+    }
+}
+else
+{
+    Console.WriteLine("Error");
+}
