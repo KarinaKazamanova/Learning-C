1ced1eb55e362eccc5e0ddf937c94839157f58fa
Karina <k5524392@gmail.com>
2022-07-09 19:45:48 +0300

    решение задачи 8

---
+++ Семинар 1/Задача 8/Program.cs
@@ -0,0 +1,9 @@
+﻿// Вывести на экран числа от -N до N (считала, что от -N до N включительно)
+Console.WriteLine("Введите число N");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = -N;
+while (i <= N)
+{
+    Console.Write(i + ", ");
+    i++;
+}
