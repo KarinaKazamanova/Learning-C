b065bc8c9bc3021094dd77096c565cf5aedcfb22
Karina <k5524392@gmail.com>
2022-07-09 20:57:24 +0300

    решение задачи 4

--- Семинар 1/Задача 4/Program.cs
+++ Семинар 1/Задача 4/Program.cs
@@ -1,36 +1,46 @@
 ﻿// По заданному с клавиатуры номеру дня недели вывести его название.
-Console.WriteLine("Введите число от 1 до 7");
-int a = Convert.ToInt32(Console.ReadLine());
-if (a == 1)
+while (true)
 {
-    Console.WriteLine("Понедельник");
+    Console.WriteLine("Введите число от 1 до 7");
+    int a = Convert.ToInt32(Console.ReadLine());
+    if (a == 1)
+    {
+         Console.WriteLine("Понедельник");
+         break;
+    }
+    else if (a == 2)
+    {
+         Console.WriteLine("Вторник");
+         break;
+    }
+    else if (a == 3)
+    {
+         Console.WriteLine("Среда");
+         break;
+    }
+    else if (a == 4)
+    {
+         Console.WriteLine("Четверг");
+         break;
+    }
+    else if (a == 5)
+    {
+         Console.WriteLine("Пятница");
+         break;
+    }
+    else if (a == 6)
+    {
+         Console.WriteLine("Суббота");
+         break;
+    }
+    else if (a == 7)
+    {
+         Console.WriteLine("Воскресенье");
+         break;
+    }
+    else
+    {
+         Console.WriteLine("Введено неверное число.");
+         continue;
+    }
 }
-else if (a == 2)
-{
-    Console.WriteLine("Вторник");
-}
-else if (a == 3)
-{
-    Console.WriteLine("Среда");
-}
-else if (a == 4)
-{
-    Console.WriteLine("Четверг");
-}
-else if (a == 5)
-{
-    Console.WriteLine("Пятница");
-}
-else if (a == 6)
-{
-    Console.WriteLine("Суббота");
-}
-else if (a == 7)
-{
-    Console.WriteLine("Воскресенье");
-}
-else
-{
-    Console.WriteLine("Введено неверное число. Введите число от 1 до 7");
-}
-
