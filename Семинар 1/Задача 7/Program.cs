0d4037c63888cb06d413c5bcea55ba3b3f286e78
Karina <k5524392@gmail.com>
2022-07-09 19:41:37 +0300

    решение задачи 7

---
+++ Семинар 1/Задача 7/Program.cs
@@ -0,0 +1,11 @@
+﻿// Выяснить является ли число чётным.
+Console.WriteLine("Введите число");
+double a = Convert.ToDouble(Console.ReadLine());
+if (a % 2 == 0)
+{
+    Console.WriteLine("Введенное число является чётным");
+}
+else
+{
+    Console.WriteLine("Введенное число не является чётным");
+}
