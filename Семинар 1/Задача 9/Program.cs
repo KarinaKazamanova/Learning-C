d680781c6126e124d58391423473c4560e7b032f
Karina <k5524392@gmail.com>
2022-07-09 16:39:01 +0300

    решение задачи 9

--- Семинар 1/Задача 9/Program.cs
+++ Семинар 1/Задача 9/Program.cs
@@ -4,9 +4,6 @@ int N = Convert.ToInt32(Console.ReadLine());
 int i = 0;
 while (i < N)
 {
-    if (i % 2 == 0)
-    {
-        Console.Write($"{i} ");
-    }
-    i++;
+    Console.Write($"{i} ");
+    i += 2;
 }
