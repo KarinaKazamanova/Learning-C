824a78351a3a62d5a3f3e52505234daedce51f87
Karina <k5524392@gmail.com>
2022-07-09 16:28:41 +0300

    решение задачи 11

---
+++ Семинар 1/Задача 11/Program.cs
@@ -0,0 +1,5 @@
+﻿// Дано число больше 9. Вывести на экран  вторую цифру числа с конца
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+int c = a/10 % 10;
+Console.WriteLine(c);
