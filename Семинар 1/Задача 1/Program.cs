d0f5dc3816fd352ed144ff86f841fab724b94ea4
Karina <k5524392@gmail.com>
2022-07-09 14:45:52 +0300

    решение задачи 1

--- Семинар 1/Задача 1/Program.cs
+++ Семинар 1/Задача 1/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = a*a;
+Console.WriteLine(b);
+
