9770f2ed11028da981149fa45ade2d1f4017c23d
Karina <k5524392@gmail.com>
2022-07-09 19:36:40 +0300

    решение задачи 6

---
+++ Семинар 1/Задача 6/Program.cs
@@ -0,0 +1,6 @@
+﻿// Написать программу вычисления значения функции y = sin(a). (Класс Math) (Считаю, что числом вводятся градусы, а не радианы)
+Console.WriteLine("Введите число");
+double a = Convert.ToDouble(Console.ReadLine());
+double angle    = Math.PI * a / 180.0;
+double b = Math.Sin(angle);
+Console.WriteLine(b);
