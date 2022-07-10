b0d98942499c394b5f1f93c85eb7609ca7389ff9
Karina <k5524392@gmail.com>
2022-07-10 15:20:13 +0300

    решение задачи 18

---
+++ Семинар 2/Задача 18/Program.cs
@@ -0,0 +1,52 @@
+﻿// По двум заданным числам проверять является ли одно квадратом другого
+
+/*
+Console.WriteLine("Введите число a");
+double a = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите число b");
+double b = Convert.ToDouble(Console.ReadLine());
+
+bool c, d, e;
+c = a*a == b;
+d = b*b == a;
+e = c || d;
+if (e && c)
+{
+    System.Console.WriteLine($"{b} квадрат {a} ");
+}
+else if (e && d)
+{
+    System.Console.WriteLine($"{a} квадрат {b} ");
+}
+else
+{
+    System.Console.WriteLine("Квадратов нет");
+}
+Пыталась решить в перерыве
+
+*/
+// Ваше решение:
+bool Check (double a, double b)
+{
+    return a*a==b;
+}
+Console.WriteLine("Введите число a");
+double a = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите число b");
+double b = Convert.ToDouble(Console.ReadLine());
+
+if (Check(a,b))
+{
+    Console.WriteLine(b + " является квадратом " + a); // пробую разные способы конкатенации
+}
+else if (Check(b,a))
+{
+    Console.WriteLine(a + " является квадратом " + b);
+}
+else 
+{
+    Console.WriteLine("Квадратов нет");
+}
+
