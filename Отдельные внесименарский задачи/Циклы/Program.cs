00506a14dc7e74205c5b820e64d48cf002b9698a
Karina <k5524392@gmail.com>
2022-07-10 15:32:11 +0300

    решение задачи с циклами

---
+++ Отдельные внесименарский задачи/Циклы/Program.cs
@@ -0,0 +1,22 @@
+﻿int i = 0;
+int N = 10;
+while (i<N) // пока универсальный
+{
+    i=i+1;
+    System.Console.WriteLine($"{i} ");
+}
+
+// цикл for
+for (i = 0; i<N; i = i+1)
+{
+    System.Console.WriteLine($"{i} ");
+}
+// do
+int a;
+do
+{
+    System.Console.WriteLine("Введите положительное число");
+    int a = Convert.ToInt32(Console.ReadLine());
+
+}
+while (a<= 0);
