f845856312b091e2962ea9a3c51d139d1120647c
Karina <k5524392@gmail.com>
2022-07-10 15:03:09 +0300

    решение задачи 16

--- Семинар 1/Задача 6/Program.cs
+++ Семинар 1/Задача 6/Program.cs
@@ -4,3 +4,23 @@ double a = Convert.ToDouble(Console.ReadLine());
 double angle = Math.PI * a / 180.0;
 double b = Math.Sin(angle);
 Console.WriteLine(b);
+
+
+/*
+тип возвращаемого значения          Имя_Функции    (аргумент с его типом)
+double                              Sin           (double x)
+{
+    return Math.Sin(x);
+}
+
+double y = Sin(3.14)                Собственная подпрогорамма (функция
+*/
+
+/*
+Функция, которая ничего не возвращает, то есть метод, поэтому Console.WriteLine(Pause); не сработает
+void Pause() (если бы void не было, то return должен быть)
+{
+    return; (в данном случае return досрочно прекращает работу программы и нижняя строчка становится недостижимой)
+    Console.WriteLine("Press any key");
+}
+*/
---
+++ Семинар 2/Задача 16/Program.cs
@@ -0,0 +1,33 @@
+﻿// Дано число. Проверить кратно ли оно 7 и 23
+
+/*
+bool b;
+b = true; // 1 
+b = false; // 0
+b = 2*2 != 5; //<, >, <=, >=, ==, !=
+int x = 14;
+b = x%7==0;
+System.Console.WriteLine(b);
+*/
+
+bool b,c,d;
+int x = 16;
+b = x%7 ==0; // простое (элементарное) условие
+c = x%23 ==0;
+d = b && c; // сложное условие
+System.Console.WriteLine(x);
+/*
+System.Console.WriteLine(b);
+System.Console.WriteLine(c);
+System.Console.WriteLine(d);
+
+System.Console.WriteLine(x%7 ==0 && x %23 ==0); // && - Укороченное И, || - Укороченное ИЛИ, ^ - Исключающее ИЛИ, ! - НЕ
+*/
+if (d)
+{
+    System.Console.WriteLine($"{x} Кратно 7 и 23");
+}
+else
+{
+    System.Console.WriteLine($"{x} Не кратно 7 и 23");
+}
