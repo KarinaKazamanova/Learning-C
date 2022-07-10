24d0cf67d9b626bad9b0eee49167b203c386bd2f
Karina <k5524392@gmail.com>
2022-07-10 16:17:41 +0300

    решение задачи 28

---
+++ Семинар 3/Задача 28/Program.cs
@@ -0,0 +1,17 @@
+﻿// Определить количество цифр в числе. Сделать подпрограмму.
+int N = Convert.ToInt32(Console.ReadLine());
+int CountDigits (int N)
+{
+    int k = 0;
+    while (N!=0)
+    {
+        k++;
+        N = N/10;
+    }
+    return k;
+
+}
+
+System.Console.WriteLine(CountDigits(N));
+
+// при решении задачи нужно постараться превратить ее в подпрограмму
