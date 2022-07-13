62ca22e13687366b75294d86a3cc475641718a46
Karina <k5524392@gmail.com>
2022-07-13 22:40:34 +0300

    решение задачи 17

---
+++ Семинар 2/Задача 17/Program.cs
@@ -0,0 +1,15 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+bool Check (double a)
+{
+    return a == 7 || a == 6;
+}
+Console.WriteLine("Введите число");
+double a = Convert.ToDouble(Console.ReadLine());
+if (Check(a))
+{
+    Console.WriteLine("Выходной");
+}
+else
+{
+    Console.WriteLine("Будний");
+}
