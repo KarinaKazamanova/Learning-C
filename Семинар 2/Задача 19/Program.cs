ada6c2c5dfd493061112804680acff6923f06781
Karina <k5524392@gmail.com>
2022-07-14 22:39:07 +0300

    решение задачи 23

--- Семинар 2/Задача 19/Program.cs
+++ Семинар 2/Задача 19/Program.cs
@@ -4,7 +4,7 @@
 
 bool x, y, f1, f2, f;
 x = true;
-y =true;
+y = true;
 f1 = !(x || y);
 f2 = !x && !y;
 System.Console.WriteLine($"{x} {y} {f1} {f2}");
---
+++ Семинар 2/Задача 23/Program.cs
@@ -0,0 +1,15 @@
+﻿// Найти расстояние между точками в пространстве 2D.
+
+Console.WriteLine("Введите координаты первой точки");
+double a = Convert.ToDouble(Console.ReadLine());
+double b = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координаты второй точки");
+double c = Convert.ToDouble(Console.ReadLine());
+double d = Convert.ToDouble(Console.ReadLine());
+double Distance (double a, double b, double c, double d)
+{
+     return Math.Sqrt(Math.Pow((c - a),2) + Math.Pow((d - b), 2));
+}
+
+
+System.Console.WriteLine($"Расстояние между двумя точками равно {Distance(a,b,c,d)}");
