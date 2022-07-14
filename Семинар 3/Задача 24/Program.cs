b5262c3583fa3f0811a4fb80b815e3d82b0b2aa4
Karina <k5524392@gmail.com>
2022-07-14 23:45:13 +0300

    решение задачи 24

---
+++ Семинар 3/Задача 24/Program.cs
@@ -0,0 +1,14 @@
+﻿//Вывести на экран таблицу квадратов чисел от 1 до N
+System.Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+int[] square = new int [N];
+int i = 0;
+for (i = 0; i<N; i = i+1)
+{
+    square[i] = i*i;
+    System.Console.Write($"{square[i]}, ");
+
+}
+
+
+
