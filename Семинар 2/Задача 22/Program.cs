40d5bdb8787456039cdf31b6b7390bce4ee7bd99
Karina <k5524392@gmail.com>
2022-07-14 23:17:46 +0300

    решение задачи 22

---
+++ Семинар 2/Задача 22/Program.cs
@@ -0,0 +1,18 @@
+﻿// Программа проверяет пятизначное число на палиндромом.
+Console.WriteLine("Введите пятизначное");
+int a = Convert.ToInt32 (Console.ReadLine());
+
+bool Palindrom (double a)
+{
+    bool c = (a/10000 == a % 10 && a/1000%10 == a/10%10);
+    return c;
+}
+
+if (Palindrom (a) == true)
+{
+    System.Console.WriteLine("Введенное число - палиндром");
+}
+else 
+{
+    System.Console.WriteLine("Введенное число - не палиндром");
+}
