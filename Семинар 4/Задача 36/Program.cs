70caa38ca8f2ad016e673ced335a8480d647792d
Karina <k5524392@gmail.com>
2022-07-16 15:58:55 +0300

    решение задачи 36

---
+++ Семинар 4/Задача 36/Program.cs
@@ -0,0 +1,20 @@
+﻿// Определить, присутствует ли в заданном массиве, некоторое число
+int N=12;
+int [] a = new int [N];
+Random random = new Random (100); // псевдослучайные числа
+
+for (int i=0; i<a.Length; i++)
+{
+    a[i]=random.Next(-9,10);
+}
+for (int i=0; i<a.Length; i++)
+{
+    System.Console.WriteLine($"{a[i], 4}");
+}
+int f=400;
+a[3] = f;
+// Линейный поиск
+int j=0;
+while (j<a.Length && a[j]!=f) j++;
+if (j==a.Length) System.Console.WriteLine("Not found");
+else System.Console.WriteLine($"Element {f} find at {j}");
