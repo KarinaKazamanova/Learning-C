b5ebda32eaedaa84e5636e5096c2c145cfd25676
Karina <k5524392@gmail.com>
2022-07-16 16:02:43 +0300

    решение задачи 39

---
+++ Семинар 4/Задача 39/Program.cs
@@ -0,0 +1,21 @@
+﻿int N=12; // изменить
+int [] a = new int [N];
+Random random = new Random (100); // псевдослучайные числа
+
+for (int i=0; i<a.Length; i++)
+{
+    a[i]=random.Next(-9,10);
+}
+for (int i=0; i<a.Length; i++)
+{
+    System.Console.WriteLine($"{a[i], 4}");
+}
+int s=0;
+for (int i=0; i<a; i=i+2)
+{
+    if (i%2!=0)
+    {
+        s=s+a[i];
+    }
+}
+System.Console.WriteLine(S);
