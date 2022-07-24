36b6b2154ce1c17c4ce1094b5abff81f1577cfcb
Karina <k5524392@gmail.com>
2022-07-24 21:19:07 +0300

    решение задачи 31

---
+++ Семинар 3/Задача 31/Program.cs
@@ -0,0 +1,16 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+double Power3 (double a)
+{
+    
+    a = Math.Pow(a, 3);
+    return a;
+}
+System.Console.WriteLine("Введите число");
+double N = Convert.ToDouble(Console.ReadLine());
+double result = 0;
+for (int i = 0; i<N; i = i+2)
+{
+    result = Power3(i);
+    System.Console.Write($"{result} "); 
+
+}
