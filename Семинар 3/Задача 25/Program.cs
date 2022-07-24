0e4843d1c063c1d2afb3fbcce3b608f80147b2f6
Karina <k5524392@gmail.com>
2022-07-24 14:29:27 +0300

    решение задачи 25

---
+++ Семинар 3/Задача 25/Program.cs
@@ -0,0 +1,19 @@
+﻿// Вывести на экран кубы чисел от 1 до N
+
+void Power3 (int a)
+{
+    int i = 0;
+    double b = 0;
+    while (i <= a)
+    {
+        b = Math.Pow(i, 3);
+        System.Console.Write($"{b} ");
+        i++;
+    }
+
+}
+
+
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+Power3(a);
