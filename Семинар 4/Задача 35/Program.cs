ecb59d368181ef32048b247c7c8b1ef583ce34cf
Karina <k5524392@gmail.com>
2022-07-24 21:42:38 +0300

    решение задачи 35

---
+++ Семинар 4/Задача 35/Program.cs
@@ -0,0 +1,11 @@
+﻿// Написать программу замены элементов массива на противоположные
+string? s=Console.ReadLine();
+string[] ss=s.Split(' ');
+int[] a=new int[ss.Length];
+for(int i=0;i<a.Length;i++)
+    a[i]=int.Parse(ss[i]);
+
+for(int i=0;i<a.Length;i++)
+    a[i] = a[i]*(-1);
+for(int i=0;i<a.Length;i++)  
+    System.Console.Write(a[i]);
