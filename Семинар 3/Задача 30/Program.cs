384f435081238d5803aa6144707a2f6b1528ac01
Karina <k5524392@gmail.com>
2022-07-24 20:56:19 +0300

    решение задачи 30

---
+++ Семинар 3/Задача 30/Program.cs
@@ -0,0 +1,17 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+int Product(int a)
+{
+    int i = 1;
+    int b = 1;
+    while (i <= a)
+    {
+        b = b*i;
+        i++;   
+    }
+    return b;
+}
+
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+int TotalProduct = Product(a);
+System.Console.WriteLine($"Произведение чисел от 1 до {a} = {TotalProduct}");
