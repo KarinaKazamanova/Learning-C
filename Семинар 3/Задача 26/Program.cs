7aec76f5829b50daa7532a2a7c9223f534a6d3f0
Karina <k5524392@gmail.com>
2022-07-24 16:04:43 +0300

    решение задачи 26

---
+++ Семинар 3/Задача 26/Program.cs
@@ -0,0 +1,18 @@
+﻿// Найти сумму чисел от 1 до А
+int Sum(int a)
+{
+    int i = 0;
+    int b = 0;
+    while (i <= a)
+    {
+        b = b + i;
+        i++;   
+    }
+    return b;
+}
+
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = 0;
+int TotalSum = Sum (a);
+System.Console.WriteLine($"Сумма от 1 до {a} = {TotalSum} ");
