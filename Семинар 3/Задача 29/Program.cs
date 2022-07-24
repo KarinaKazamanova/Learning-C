ec86c2e2f2550727209194fc78b2f4b5b0c3dbac
Karina <k5524392@gmail.com>
2022-07-24 16:50:05 +0300

    решение задачи 29

---
+++ Семинар 3/Задача 29/Program.cs
@@ -0,0 +1,20 @@
+﻿// Подсчитать сумму цифр в числе
+int CountDigits (int N)
+{
+    if (N==0) return 0;
+    int k = 0;
+    int b = 0;
+    int a = 0;
+    while (N!=0)
+    {
+        k++;
+        a = N%10;
+        N = N/10;
+        b = b + a;
+    }
+    return b;
+}
+Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+int Sum = CountDigits (N);
+System.Console.WriteLine($"Сумма чисел введенного числа равна {Sum}");
