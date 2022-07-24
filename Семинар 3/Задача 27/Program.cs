3b06c69b16f76acc34e7321a6643a0e842a018b0
Karina <k5524392@gmail.com>
2022-07-24 16:20:59 +0300

    решение задачи 27

---
+++ Семинар 3/Задача 27/Program.cs
@@ -0,0 +1,36 @@
+﻿// Возведите число А в натуральную степень B используя цикл
+
+double Pow(double a, int power)
+{
+    int i =0;
+    double b = 1;
+    if (power > 0)
+    {
+        while (i < power)
+        {
+             b = b * a;
+             i++;
+        }
+    }
+    else if (power < 0)
+    {
+        while (i > power)
+        {
+             b = b / a;
+             i = i - 1;
+        }
+
+    }
+    else if (power == 1)
+    {
+        b = a;
+    }
+
+    return b;
+}
+Console.WriteLine("Введите число");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("В какую степень хотите возвести введенное число (введите целое число, пожалуйста, не помню, как возводить в дробные:(. Можно отрицательные )?");
+int power = Convert.ToInt32(Console.ReadLine());
+double Result = Pow(a, power);
+System.Console.WriteLine($"{a} в степени {power} = {Result}");
