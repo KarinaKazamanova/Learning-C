d4357b1ed2b023468e88371356165a44bedd0ccf
Karina <k5524392@gmail.com>
2022-07-30 20:52:22 +0300

    решение задачи 44

---
+++ Семинар 5/Задача 44/Program.cs
@@ -0,0 +1,17 @@
+﻿// Написать программу преобразования десятичного числа в двоичное
+int Binar (int a)
+{
+    int binar = 0;
+    double i = 0;
+    while (a != 0)
+    {
+        binar = binar + a%2 * Convert.ToInt32(Math.Pow(10, i));
+        a = a/2;
+        i++;
+    }
+    return binar;
+}
+System.Console.WriteLine("Введите число n");
+int n=Convert.ToInt32(Console.ReadLine());
+int b = Binar (n);
+System.Console.WriteLine($"{n} = {b} в двоичной системе исчисления");
