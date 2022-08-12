4dd086b9816cd6b71934bc97a90f4f02fd09165f
Karina <k5524392@gmail.com>
2022-08-12 22:34:47 +0300

    решение задачи 004

--- EJUDGE/Array1/004/Program.cs
+++ EJUDGE/Array1/004/Program.cs
@@ -2,10 +2,11 @@
 Определить, сколько среди них пар одинаковых чисел.
 100>=N>=2
 */
-int n = int.Parse(System.Console.ReadLine());
-string  s=System.Console.ReadLine();
-string [] ss = s.Split(' ', StringSplitOptions.RemoveEmptyEntries);
-int[] a = System.Array.ConvertAll<string, int>(ss, int.Parse); // первый параметр - из какого типа, второй параметр - в какой тип мы хотим изменить данное
+using System;
+int n = int.Parse(Console.ReadLine());
+string  s=Console.ReadLine();
+string [] ss = s.Split(' ',StringSplitOptions.RemoveEmptyEntries);
+int[] a = Array.ConvertAll<string, int>(ss, int.Parse); // первый параметр - из какого типа, второй параметр - в какой тип мы хотим изменить данное
 int k = 0;
 for (int i = 0; i < a.Length; i++)
     for (int j=i +1;j< a.Length; j++)
