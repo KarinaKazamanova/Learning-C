9152aadbf10105af55388081c2c2ccbaab80a6ed
Karina <k5524392@gmail.com>
2022-08-12 22:17:37 +0300

    решение задачи 004

---
+++ EJUDGE/Array1/004/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Вводится число N, а затем - N чисел.
+Определить, сколько среди них пар одинаковых чисел.
+100>=N>=2
+*/
+int n = int.Parse(Console.ReadLine());
+string  s=Console.ReadLine();
+string [] ss = s.Split(' ');
+int[] a = Array.ConvertAll<string, int>(ss, int.Parse); // первый параметр - из какого типа, второй параметр - в какой тип мы хотим изменить данное
+int k = 0;
+for (int i = 0; i < a.Length; i++)
+    for (int j=i +1;j< a.Length; j++)
+    if (a[i] == a[j])
+        k++;
+
+System.Console.WriteLine(k);
