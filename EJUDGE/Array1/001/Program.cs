fd68c1bcfc0c38fd99a404448ce55b9ae552a719
Karina <k5524392@gmail.com>
2022-08-12 22:54:39 +0300

    решение задачи 001

---
+++ EJUDGE/Array1/001/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Вводится сначала число N, а затем N чисел. Выведите эти N чисел в обратном порядке.
+
+Входные данные
+Вводится число N (100>N>0), а затем N чисел из диапазона Integer.
+
+Выходные данные
+Выведите N чисел в обратном порядке
+*/
+using System;
+int n = int.Parse(Console.ReadLine());
+string  s=Console.ReadLine();
+string [] ss = s.Split(' ',StringSplitOptions.RemoveEmptyEntries);
+int[] a = Array.ConvertAll<string, int>(ss, int.Parse);
+int temp = 0;
+for (int i =0; i < a.Length/2; i++)
+{
+    temp = a[i]; 
+    a[i] = a[a.Length -i -1];
+    a[a.Length -i -1] = temp;
+}
+for (int i =0; i < a.Length; i++)
+    System.Console.Write($"{a[i]} ");
