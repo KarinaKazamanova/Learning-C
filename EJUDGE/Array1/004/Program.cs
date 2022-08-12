c56f38482f1e3fef726ed3be4ebdf3381ada685e
Karina <k5524392@gmail.com>
2022-08-12 22:33:39 +0300

    решение задачи 004

--- EJUDGE/Array1/004/Program.cs
+++ EJUDGE/Array1/004/Program.cs
@@ -3,8 +3,8 @@
 100>=N>=2
 */
 int n = int.Parse(System.Console.ReadLine());
-string  s=System.Console.ReadLine().Trim();
-string [] ss = s.Split(' ');
+string  s=System.Console.ReadLine();
+string [] ss = s.Split(' ', StringSplitOptions.RemoveEmptyEntries);
 int[] a = System.Array.ConvertAll<string, int>(ss, int.Parse); // первый параметр - из какого типа, второй параметр - в какой тип мы хотим изменить данное
 int k = 0;
 for (int i = 0; i < a.Length; i++)
