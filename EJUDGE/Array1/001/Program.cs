564c1f96e0351803f069a1c4d33676279478b15f
Karina <k5524392@gmail.com>
2022-08-13 00:10:17 +0300

    решение задачи 002

--- EJUDGE/Array1/001/Program.cs
+++ EJUDGE/Array1/001/Program.cs
@@ -8,15 +8,9 @@
 */
 using System;
 int n = int.Parse(Console.ReadLine());
-string  s=Console.ReadLine();
+string s =Console.ReadLine();
 string [] ss = s.Split(' ',StringSplitOptions.RemoveEmptyEntries);
-int[] a = Array.ConvertAll<string, int>(ss, int.Parse);
-int temp = 0;
-for (int i =0; i < a.Length/2; i++)
-{
-    temp = a[i]; 
-    a[i] = a[a.Length -i -1];
-    a[a.Length -i -1] = temp;
-}
-for (int i =0; i < a.Length; i++)
-    System.Console.Write($"{a[i]} ");
+string [] b = ss.Reverse;
+
+for (int i=0; i < b.Length; i++)
+    System.Console.WriteLine(b[i]);
---
+++ EJUDGE/Array1/002/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Вводится сначала число N, а затем N чисел. Выведите эти N чисел в следующем порядке: сначала выводятся все нечетные числа в том порядке, в каком они встречались во входном файле, а затем - все четные. Входные данные
+Вводится число N (100>N>0), а затем N чисел из диапазона Integer
+*/
+using System;
+double n = double.Parse(Console.ReadLine());
+string  s=Console.ReadLine();
+string [] ss = s.Split(' ',StringSplitOptions.RemoveEmptyEntries);
+double[] a = Array.ConvertAll<string, double>(ss, double.Parse);
+
+for (int i = 0; i < a.Length; i ++)
+    if (a[i]%2 !=0)
+        System.Console.Write($"{a[i] }");
+for (int i = 0; i < a.Length; i ++)
+    if (a[i]%2 ==0)
+        System.Console.Write($"{a[i] }");
+   
