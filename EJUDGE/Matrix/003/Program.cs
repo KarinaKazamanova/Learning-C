f1ab8844049d1efa1439bcaa3142217f628f8908
Karina <k5524392@gmail.com>
2022-08-13 23:37:18 +0300

    решение задачи 003

---
+++ EJUDGE/Matrix/003/Program.cs
@@ -0,0 +1,18 @@
+﻿string s = System.Console.ReadLine();
+int N = int.Parse(s.Split(' ')[0]);
+int M = int.Parse(s.Split(' ')[1]);
+int [,] a = new int[N,M];
+for (int i= 0;i<N;i++)
+{
+    s=Console.ReadLine();
+    string[] ss= s.Split(' ',StringSplitOptions.RemoveEmptyEntries);
+    for (int j = 0; j <ss.Length; j++)
+        a[i,j] = int.Parse(ss[j]);
+
+}
+int sum = 0;
+for (int i= 0;i<a.GetLength(0);i++)
+{
+    sum = sum + a[i,i];
+}
+System.Console.WriteLine(sum);
