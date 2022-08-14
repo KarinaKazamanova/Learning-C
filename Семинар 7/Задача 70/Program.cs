b2161070c091a4785c0e7fa6408834c965639a1a
Karina <k5524392@gmail.com>
2022-08-14 10:54:03 +0300

    решение задачи 70

--- Семинар 7/Задача 70/Program.cs
+++ Семинар 7/Задача 70/Program.cs
@@ -1,11 +1,11 @@
 ﻿// Найти сумму цифр числа - 2
 Random random = new Random();
 
-int N = random.Next(1,101);
+int N = random.Next(1,100001);
 
 int SumInt (int N)
 {
-    if (N==0) return N;
+    if (N<10) return N;
     else return SumInt (N/10) + N%10;
 }
 System.Console.WriteLine(N);
