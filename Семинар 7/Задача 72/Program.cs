d231a8f213805f87bad5a42c980d32df18c37920
Karina <k5524392@gmail.com>
2022-08-14 10:37:43 +0300

    решение задачи 72

---
+++ Семинар 7/Задача 72/Program.cs
@@ -0,0 +1,15 @@
+﻿// Написать программу возведения числа А в целую степень B - 3
+
+Random random = new Random();
+
+int N = random.Next(1,11);
+int M = random.Next(1,11);
+
+int Power (int n, int m)
+{
+    if (m==0) return 1;
+    if (m%2==0) return Power (n,m/2)*Power (n,m/2);
+    else return Power (n,m-1)*n;
+}
+System.Console.Write($"{N} в степени {M} = ");
+System.Console.WriteLine(Power (N,M));
