a4a7440d0312eaa5688292805a24186019d47446
Karina <k5524392@gmail.com>
2022-08-14 11:29:38 +0300

    решение задачи 73

---
+++ Семинар 7/Задача 73/Program.cs
@@ -0,0 +1,22 @@
+﻿// Написать программу показывающие первые N чисел, для которых каждое следующее равно сумме двух предыдущих. Первые два элемента последовательности 0 и 1 - 4
+
+Random random = new Random();
+int N = random.Next(1,15);
+
+System.Console.WriteLine(N);
+System.Console.WriteLine($"Последовательность Фибоначи для {N} введенных чисел");
+Febonacci (N);
+for (int i = 0; i <= N; i++)
+    {
+        System.Console.WriteLine(Febonacci(i));
+    }
+int Febonacci (int N)
+{
+    if (N==0 || N==1) return 1;
+    else 
+    {
+        return Febonacci (N-1) + Febonacci (N-2); 
+        
+    }
+    
+}
