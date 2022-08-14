b47d6dffed18185cef3408313f87103e30ea0e30
Karina <k5524392@gmail.com>
2022-08-14 10:10:05 +0300

    решение задачи 69

---
+++ Семинар 7/Задача 69/Program.cs
@@ -0,0 +1,19 @@
+﻿// Найти сумму элементов от M до N, N и M заданы - 1
+Random random = new Random();
+
+int N = random.Next(1,11);
+int M = random.Next(1,11);
+if (N > M) 
+    {
+        int t;
+        t=N;
+        N=M;
+        M=t;
+    } 
+int SumRec(int n, int m)
+{
+    if (n < m) return SumRec (n+1,m) + n;
+    else return n;
+}
+System.Console.WriteLine($" ({N}, {M})");
+System.Console.WriteLine(SumRec(N,M));
