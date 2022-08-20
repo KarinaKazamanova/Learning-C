f817c125461d6d49be686c6d3657fbc91c68e05d
Karina <k5524392@gmail.com>
2022-08-20 14:36:06 +0300

    char

---
+++ Семинар 9/Char/Program.cs
@@ -0,0 +1,20 @@
+﻿char c;
+System.Char c1; // то же, что и в строчке 1 , UTF - 16 => 1 символ = 2 Байта
+c = 'a'; // Для с# 'char' и "string" разный тип данных, соответвенно разные кавычки
+System.Console.WriteLine($"{c} {(int)c}");
+for (char c2 = 'a'; c2<='z'; c2++)
+    System.Console.WriteLine(c2);
+
+if (c>='A' && c<='Z')
+{
+    System.Console.WriteLine("Big latin char");
+}
+
+if (c>='0' && c<='9')
+{
+    System.Console.WriteLine("char is digit");
+}
+if (char.IsDigit(c))
+{
+    System.Console.WriteLine("char is digit");
+}
