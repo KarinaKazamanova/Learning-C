17b5afe34bc156daa6d824f3b777f595443ba61b
Karina <k5524392@gmail.com>
2022-08-21 11:22:54 +0300

    решение задачи 65

---
+++ Семинар 9/Задача 65 А/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+System.Console.WriteLine("Введите число M");
+int m = int.Parse(Console.ReadLine());
+System.Console.WriteLine("Введите число N");
+int n = int.Parse(Console.ReadLine());
+
+
+if (m>n)
+{
+    int temp = 0;
+    temp = m;
+    m = n;
+    n = temp;
+}
+System.Console.WriteLine(Sum (m, n));
+
+int Sum (int m, int n)
+{
+    int sum =0;
+    if (m==n) return sum = sum + n;
+    else return sum = sum + Sum(m +1, n) +m;
+}
