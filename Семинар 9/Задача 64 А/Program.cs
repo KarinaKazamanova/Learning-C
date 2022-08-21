aa78ef41e97f5b44f7871c28da8a708ad7012a43
Karina <k5524392@gmail.com>
2022-08-21 11:15:32 +0300

    решение задачи 64

---
+++ Семинар 9/Задача 64 А/Program.cs
@@ -0,0 +1,22 @@
+﻿//  Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+System.Console.WriteLine("Введите число M");
+int m = int.Parse(Console.ReadLine());
+System.Console.WriteLine("Введите число N");
+int n = int.Parse(Console.ReadLine());
+
+if (m>n)
+{
+    int temp = 0;
+    temp = m;
+    m = n;
+    n = temp;
+}
+
+void Numbers(int m, int n)
+{
+    if (m==n-1) System.Console.Write($"{m} ");
+    else  Numbers (m, n-1); System.Console.Write($"{n} ");
+}
+
+Numbers (m, n);
