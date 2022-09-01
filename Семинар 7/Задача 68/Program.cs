ef4be4d16378c11494b32858c109987087e037aa
Karina <k5524392@gmail.com>
2022-09-01 22:42:34 +0300

    решение задачи 68

---
+++ Семинар 7/Задача 68/Program.cs
@@ -0,0 +1,20 @@
+﻿// Показать натуральные числа от M до N, N и M заданы
+
+System.Console.WriteLine("Введите число M");
+int M = int.Parse(Console.ReadLine());
+System.Console.WriteLine("Введите число N");
+int N = int.Parse(Console.ReadLine());
+
+if (M>N)
+{
+    int temp = 0;
+    temp = M;
+    M = N;
+    N = temp;
+}
+Numbers (M, N);
+void Numbers(int m, int n)
+{
+    if (m==n-1) System.Console.Write($"{m} ");
+    else  Numbers (m, n-1); System.Console.Write($"{n} ");
+}
