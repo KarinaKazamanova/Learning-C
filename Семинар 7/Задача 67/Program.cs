a143388b8d7d55caea13ff60ac385c37c40bc25e
Karina <k5524392@gmail.com>
2022-09-01 22:38:08 +0300

    решение задачи 67

---
+++ Семинар 7/Задача 67/Program.cs
@@ -0,0 +1,14 @@
+﻿// Показать натуральные числа от N до 1, N задано
+System.Console.WriteLine("Введите число");
+int M = int.Parse(System.Console.ReadLine());
+Numbers (M);
+void Numbers (int N)
+{
+    if (N == 1) System.Console.Write($"{N} ");
+    else 
+    {
+        System.Console.Write($"{N} ");
+        Numbers(N-1); 
+        
+    } 
+}
