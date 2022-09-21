39e84bb6fb48776582fba59e4333ad67f885de29
Karina <k5524392@gmail.com>
2022-09-21 23:16:30 +0300

    Решение задачи 65

--- Семинар 6/Задача 65/Program.cs
+++ Семинар 6/Задача 65/Program.cs
@@ -10,38 +10,163 @@ int [,] Spiral ( int M, int N)
 {
     int [,] Mtx = new int [M,N];
     int k = 1;
-    for (int i = 0; i < M; i++)
+    int count = N;
+    
+    for (int i = 0; i < (M)/2; i++)
     {
-        for (int j = 0; j < N; j ++)
+        for ( int j = i; j <= count - i; j ++)
         {
-            while (true)
-            {
-                if ( Mtx [i, j +1] != 0)
-                {
-                    int temp = 0;
-                    temp = i;
-                    i = j;
-                    j = temp;
-                    
-                    
-                }
-                else 
+            
+                
+                if (j == (count-1-i))
                 {
                     Mtx [i, j] = k;
                     k = k+1;
+                    Mtx = Transp(Mtx);
+                    if (count == N)
+                    {
+                        count = M;
+                        j = i;
+                    }
+                    else 
+                    {
+                        count = N;
+                        j = i;
+                    } 
+                    continue;
                 }
-            }
+                else if (j == (count-2-i) &&  Mtx [i, j +1] != 0)
+                {
+                    Mtx [i, j] = k;
+                    k = k+1;
+                    Mtx = Transp(Mtx);
+                    if (count == N)
+                    {
+                        count = M;
+                        j = i;
+                        
+                        
+                    }
+                    else 
+                    {
+                        count = N;
+                        j = i;
+                    } 
+                    break;
+                }
+                else if (j < (count-1-i) &&  Mtx [i, j +1] == 0 )
+                {
+                    Mtx [i, j] = k;
+                    k = k+1;
+                    
+                }
+                
+                
+                
         }
+            
+         
     }
     return Mtx;
+                
 }
+    
 void Print(int[,] a)
 {
     for (int i=0; i<a.GetLength(0); i++)
     {
         for (int j=0; j<a.GetLength(1);j++)
-        if (a[i,j] != 0) System.Console.Write($"{a[i,j],2} ");
+        if (a[i,j] != 0) System.Console.Write($"{a[i,j],4} ");
         else System.Console.Write($"   ");
     System.Console.WriteLine();
     }
 }
+int [,] Transp(int [,]M )
+{
+    int [,] N = new int [M.GetLength(1), M.GetLength(0)];
+    for (int i = 0; i < M.GetLength(1); i++)
+    {
+        for (int j = 0; j < M.GetLength(0); j++)
+        {
+            N [i,j] = M [j,M.GetLength(1)-1-i];
+        }
+    }
+    return N;
+
+}
+
+/*int [,] Spiral ( int M, int N)
+{
+    int [,] Mtx = new int [M,N];
+    int k = 1;
+    int count = N;
+    for (int i = 0; i < M-1; i++)
+    {
+        for (int j = 0; j < count; j ++)
+        {
+            
+                if (j < (count-1) &&  Mtx [i, j +1] == 0 )
+                {
+                    Mtx [i, j] = k;
+                    k = k+1;
+                }
+                else if (j == (count-1))
+                {
+                     Mtx [i, j] = k;
+                    k = k+1;
+                    Mtx = Transp(Mtx);
+                    if (count == N)
+                    {
+                        count = M;
+                        j = i;
+                    }
+                    else 
+                    {
+                        count = N;
+                        j = i;
+                    } 
+                    continue;
+                }
+                else if (j != (count-1) && Mtx [i, j +1] != 0)
+                {
+                    Mtx [i, j] = k;
+                    k = k+1;
+                    Mtx = Transp(Mtx);
+                    if (count == N)
+                    {
+                        count = M;
+                        
+                        j = i;
+                    }
+                    else 
+                    {
+                        count = N;
+                        j = i;
+                    } 
+                    break;
+                
+                }
+                else if (j == (count-2) &&  Mtx [i, j +1] != 0)
+                {
+                    Mtx = Transp(Mtx);
+                    if (count == N)
+                    {
+                        count = M;
+                        
+                        j = i;
+                    }
+                    else 
+                    {
+                        count = N;
+                        j = i;
+                    } 
+                    continue;
+                }
+        }
+            
+         
+    }
+    return Mtx;
+                
+}
+*/
